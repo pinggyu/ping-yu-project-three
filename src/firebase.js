df27b505f1584cc7f5a6ddef912aac097fc786ef
Ping Yu <pingyuuu@hotmail.com>
2022-08-11 00:03:21 -0400

    mvp done with firebase connection and base styline

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAvZ-N_OrijNPpyRwRQoUWXedObrfsdD9E",
+  authDomain: "travelsheets-f52a0.firebaseapp.com",
+  projectId: "travelsheets-f52a0",
+  storageBucket: "travelsheets-f52a0.appspot.com",
+  messagingSenderId: "385051987864",
+  appId: "1:385051987864:web:fc57081d8ca725fbe225ed"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
