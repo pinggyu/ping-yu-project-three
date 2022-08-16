9189e123a2ac1cee6f4d8c84ce52398d7685aa5c
Ping Yu <pingyuuu@hotmail.com>
2022-08-16 00:04:05 -0400

    setup routing for netlify

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,15 @@
 function Footer() {
   return (
     <footer>
-    <p>
-        © 2022 - Made by <a className='footerLink' href="https://github.com/pinggyu"> Ping Yu </a> at Juno College
-    </p>
-    <p className="credits">
-      Images provided by <a className='footerLink' href="https://unsplash.com/developers"> Unsplash API. </a>
-      Header background provided by <a className='footerLink' href="https://loading.io/"> loading.io </a>
-    </p>
+      <p>
+          © 2022 - Made by <a className='footerLink' href="https://github.com/pinggyu"> Ping Yu </a> at Juno College
+      </p>
+      <p className="credits">
+        Images provided by <a className='footerLink' href="https://unsplash.com/developers"> Unsplash API</a> and profanity filter by <a className='footerLink' href="https://www.purgomalum.com/index.html">PurgoMalum RESTful API.</a>
+      </p>
+      <p className="credits">
+        Header background provided by <a className='footerLink' href="https://loading.io/"> loading.io </a>
+      </p>
     </footer>
   )
 }
